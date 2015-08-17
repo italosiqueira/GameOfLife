8e11fbb5b089dc2f8c8993d97bc52549ca46ebac
italosiqueira <italosiqueira@gmail.com>
2015-08-16 23:28:12 -0300

    Added navigation by arrow keys.

--- Conway/js/life.js
+++ Conway/js/life.js
@@ -108,6 +108,7 @@ function $(selector, container) {
                     checkbox.type = 'checkbox';
 
                     this.checkboxes[y][x] = checkbox;
+                    checkbox.coords = [y, x];
 
                     cell.appendChild(checkbox);
                     row.appendChild(cell);
@@ -119,6 +120,38 @@ function $(selector, container) {
             this.grid.addEventListener('change', function (evt) {
                 if (evt.target.nodeName.toLowerCase() == 'input') {
                     me.started = false;
+                }
+            });
+
+            this.grid.addEventListener('keyup', function (evt) {
+                var checkbox = evt.target;
+                if (evt.target.nodeName.toLowerCase() == 'input') {
+                    var coords = checkbox.coords;
+                    var y = coords[0];
+                    var x = coords[1];
+
+                    switch (evt.keyCode) {
+                        case 37: // left
+                            if (x > 0) {
+                                me.checkboxes[y][x - 1].focus();
+                            }
+                            break;
+                        case 38: // up
+                            if (y > 0) {
+                                me.checkboxes[y - 1][x].focus();
+                            }
+                            break;
+                        case 39: //right
+                            if (x < me.size - 1) {
+                                me.checkboxes[y][x + 1].focus();
+                            }
+                            break;
+                        case 40: // bottom
+                            if (y < me.size - 1) {
+                                me.checkboxes[y + 1][x].focus();
+                            }
+                            break;
+                    };
                 }
             });
 
